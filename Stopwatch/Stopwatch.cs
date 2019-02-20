a58fee4ab5c98e3f354de43232acea1cad776274
janLoveBuffet <jan314052@gmail.com>
2019-02-20 23:58:41 +0700

    test

---
+++ Stopwatch/Stopwatch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        public static void Main(string[] args)
+        {
+            int s = 0, m = 0, h = 0;
+            // DateTime Timestamp = DateTime.Now;
+            TimeSpan Timestamp = new TimeSpan(0, 1, 10);
+            TimeSpan Time = new TimeSpan(0, 0, 50);
+            double TimestampInt = Timestamp.TotalSeconds;
+            double TimeInt = Time.TotalSeconds;
+
+            while (TimeInt < TimestampInt)
+            {
+                Thread.Sleep(1000);
+                var currentTime = Time.Add(new TimeSpan(0, 0, ++s));
+                Console.Write("\r" + currentTime);
+                TimeInt++;
+            }
+        }
+    }
+}
